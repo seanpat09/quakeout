9b693b0b0c9e4d56876e8b2443307fc2c8cfaff1
Sean Cuevo <seanpat09@gmail.com>
2012-09-05 14:01:25 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quakeout::Application.config.secret_token = '19fcd571c600b714ff6d1576b82e0029a55e982d8551850f8f513b481adbe1430fce61b0f9cbcf27ecc515257a74299fbd86006731569a8966a858371deb8849'
