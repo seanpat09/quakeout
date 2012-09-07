b90e3aa85c7239a5af9e0547f6505375e922cb67
Sean Cuevo <seanpat09@gmail.com>
2012-09-07 09:59:00 -0400

    Fixed popup data repitition, added correct data

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,13 +1,15 @@
 var map
 var geocoder
+var infowindow
 $(function() {
   var geocoder = new google.maps.Geocoder();
   var mapOptions = {
-    center: new google.maps.LatLng(-34.397, 150.644),
-    zoom: 0,
+    center: new google.maps.LatLng(0, 0),
+    zoom: 1,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  map = new google.maps.Map($("#map_canvas").get(0), mapOptions);
+  map = new google.maps.Map($('#map_canvas').get(0), mapOptions);
+  infowindow = new google.maps.InfoWindow();
 });
 
 function center(lat,lng) {
@@ -19,51 +21,49 @@ function markMap(lat,lng){
   alert('hello');
 };
 
-function codeAddress(address) {
+function makePopup(marker, details){
+  return function () {
+    infowindow.close();
+    infowindow = new google.maps.InfoWindow();
+    infowindow.setContent(details);
+    infowindow.open(map,marker);
+  };
+};
+
+function placeMarkers(address) {
   geocoder.geocode( { 'address': address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       map.fitBounds(results[0].geometry.viewport);
       var ne = results[0].geometry.viewport.getNorthEast();
       var sw = results[0].geometry.viewport.getSouthWest();
 
-      var earthquake_request = 'http://api.geonames.org/earthquakesJSON?north=' + ne.lat() + '&south=' + sw.lat() + '&east=' + ne.lng() + '&west=' + sw.lng() + '&username=demo';
-
-      var infowindow = new google.maps.InfoWindow();
+      var geonames_request = 'http://api.geonames.org/earthquakesJSON?north=' + ne.lat() + '&south=' + sw.lat() + '&east=' + ne.lng() + '&west=' + sw.lng() + '&username=quakeout';
 
       var marker, i;
 
-      $.getJSON(earthquake_request, function(data) {
+      $.getJSON(geonames_request, function(data) {
         var earthquakes = data.earthquakes;
         for(i=0; i < earthquakes.length; i++){
           marker = new google.maps.Marker({
             map: map,
             position: new google.maps.LatLng(earthquakes[i].lat, earthquakes[i].lng)
           });
-        var details =  '<div id="content">'+
-    '<div id="siteNotice">'+
-    '</div>'+
-    '<h2 id="firstHeading" class="firstHeading">Uluru</h2>'+
-    '<div id="bodyContent">'+
-    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-    'sandstone rock formation in the southern part of the '+
-    'Northern Territory, central Australia. It lies 335 km (208 mi) '+
-    'south west of the nearest large town, Alice Springs; 450 km '+
-    '(280 mi) by road. Kata Tjuta and Uluru are the two major '+
-    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-    'Aboriginal people of the area. It has many springs, waterholes, '+
-    'rock caves and ancient paintings. Uluru is listed as a World '+
-    'Heritage Site.</p>'+
-    '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-    'http://en.wikipedia.org/w/index.php?title=Uluru</a> (last visited June 22, 2009).</p>'+
-    '</div>'+
-    '</div>';
-          google.maps.event.addListener(marker, 'click', (function(marker, i){
-            return function () {
-              infowindow.setContent(details);
-              infowindow.open(map,marker);
-            }
-          })(marker, i));
+
+          var details =  '<div id="content">'+
+             '<div id="siteNotice">'+
+            '</div>'+
+            '<h2 id="firstHeading" class="firstHeading">Earthquake Details</h2>'+
+            '<div id="bodyContent">'+
+            '<p><b>Date and Time (YYYY-MM-DD): </b>' + earthquakes[i].datetime + '</p>' +
+            '<p><b>Location (lat,lng):</b> (' + earthquakes[i].lat + ',' + earthquakes[i].lng +')</p>' +
+            '<p><b>Magnitude:</b> ' + earthquakes[i].magnitude + '</p>' + 
+            '<p><b>Depth:</b> ' + earthquakes[i].depth + '</p>' +
+            '<p>Information courtesy of GeoNames JSON Webservices: <a href="http://www.geonames.org/export/JSON-webservices.html">'+
+            'http://www.geonames.org/export/JSON-webservices.html</a>.</p>'+
+ 
+            '</div>'+
+            '</div>';
+          google.maps.event.addListener(marker, 'click', makePopup(marker,details));
         };         
       });
 
